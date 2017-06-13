90a4e89d96fce76b503e8f1b5c56266c6a0dbb31
Romi RB <rominana1234@gmail.com>
2017-06-13 01:01:02 -0300

    Create addremove.js

--- jquery/addremove.js
+++ jquery/addremove.js
@@ -1,9 +1,32 @@
+//Añadir o quitar clases...
+
+//-------------addClass-----------------//
+//Sirve para que el elemento que recibe el método se le aplique una clase CSS //
+
+//------------removeClass--------------//
+//Sirve para que el elemento que recibe el método  se le elimine una clase CSS //
+
+//------------EVENTOS----------------que se asocian a enlaces: //
+
+//-----------mouseover--------------//
+//Cuando el usuario coloque el puntero sobre el enlace
+
+//---------mouseout-------------//
+//Cuando el usuario retire el ratón del enlace
+
+//------------------------------------------------------------------------//
 
 $(document).ready(function(){
-
+	
+//---------------evento para cuando el usuario pasa el puntero por el enlace----------------//
+	
 	$('a').mouseover(function(event){
-		$('#capa').addClass('clasecss');
-	});
+		$('#capa').addClass('clasecss'); //a addClass se le pasa una cadena con el nombre de la clase CSS ('clasecss')
+						//para seleccionar el elemento que queremos añadir la clase hacemos $('#capa')estamos 
+	});					//seleccionando el elemento de la página cuyo id="capa"
+	
+//---------------evento para cuando el usuario saca el puntero del enlace----------------//
+	
 	$('a').mouseout(function(event){
 		$('#capa').removeClass('clasecss');
 	});
